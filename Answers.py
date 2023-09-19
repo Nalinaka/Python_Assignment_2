77901493a3b749b63828f222e8571d95006fdca5
Nalinaka <nalinaka@gmail.com>
2023-09-18 20:43:46 -0700

    Updated multi-list code

--- Answers.py
+++ Answers.py
@@ -17,21 +17,22 @@ print("The maximum of these numbers is:", result)
 
 
 def mult_list(lst):
-    
-    if len(lst) == 0:
-        return 0
+  #if empty list, return 0
+  if len(lst) == 0:
+    return 0
+  #product starts with first element of list
+  prod = lst[0]
 
-    prod = lst[0]
+  #go through list elements and multiply them together
+  if len(lst) > 1:
     for i in lst[1:]:
-        prod = prod * i
+      prod = prod * i
 
-        return prod
-
-
-#test
-    print(mult_list([2,3,4]))
-    print(mult_list([]))
-    print(mult_list([10]))
+  return prod
+    
+print(mult_list([1,2,3]))
+print(mult_list([]))
+print(mult_list([15]))
 
 
 
